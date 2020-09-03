f7091640bb9d67a98286a6d311a2eb5dcfaa1509
Njeririziki <rizikinjeri@gmail.com>
2020-09-03 23:28:03 +0300

    crud

--- Server.js
+++ Server.js
@@ -1,13 +1,23 @@
 const express = require ("express")
 const bodyParser = require('body-parser')
 const app= express()
+const MongoClient = require('mongodb').MongoClient
+const url = 'mongodb://localhost:27017/transactions'
 
 
-app.get('/', (req,res) =>{
-    res.send('started')
-}
-  );
 
-    app.listen(3000,()=>{
-        console.log('server up')
-    })
+
+   MongoClient.connect(url,(err,db)=>{
+
+     if(err) return console.log(err)
+     const daily= db.db('transactions');
+     const firstdoc = { activity:"Breakfast", cost:"200"}
+     daily.collection('activities').deleteOne( firstdoc,(err,res)=>{
+       if(err) throw err;
+       console.log('Document deleted');
+       db.close()
+     });
+   
+   })
+
+   
